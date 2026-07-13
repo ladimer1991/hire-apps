d04067805c9b8b199412b4d6c1402dd6db38c3d0
ladimer <main@MacBookPro.lan>
2026-07-13 14:31:48 -0700

    Add firebase file: google-services.json

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.googleServices)
 }
 
 kotlin {
@@ -18,6 +19,11 @@ dependencies {
 
     implementation(libs.compose.uiToolingPreview)
     debugImplementation(libs.compose.uiTooling)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.messaging)
 }
 
 android {
